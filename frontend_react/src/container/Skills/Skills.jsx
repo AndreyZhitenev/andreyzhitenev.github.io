9a601a27bdec2e9aa5e4e6c33195f7efa108c895
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-08-13 19:51:48 +0300

    changed content

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -34,11 +34,11 @@ const Skills = () => {
 	}, []);
 
 	experiencesEn.sort((a, b) => {
-		return b.year.split("-")[0] - a.year.split("-")[0];
+		return b.year.split(" ")[1] - a.year.split(" ")[1];
 	});
 
 	experiencesRu.sort((a, b) => {
-		return b.year.split("-")[0] - a.year.split("-")[0];
+		return b.year.split(" ")[1] - a.year.split(" ")[1];
 	});
 
 	return (
